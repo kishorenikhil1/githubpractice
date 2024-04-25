85417a047f23bb2b59c560c3bab062846d40f13e
root <root@ip-172-31-6-94.us-east-2.compute.internal>
2024-04-25 06:28:50 +0000

    temp code

--- demo.java
+++ demo.java
@@ -1,3 +1,14 @@
+class Temp
+{
+	void Demo_code()
+	{
+		s.o.print("test code")
+		s.o.print("test code")
+		s.o.print("test code")
+		s.o.print("test code")
+		s.o.print("test code")
+	}
+}
 class demo.java
 {
 	public static void main()
