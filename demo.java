5d9b9c5cbda17355060684610cd1cb58e6902e5b
kishorenikhil1 <kishorenikhil1@gmail.com>
2024-04-28 08:44:11 +0000

    java code

--- demo.java
+++ demo.java
@@ -1,42 +1,4 @@
-class Bank
-{
-	void deposite()
-		bal =bal+amount()
-
-}
-	void withdraw()
-{
-	bal = bal-amt
-}
-}
-
-
-
-class Temp
-{
-	void Demo_code()
-	{
-		s.o.print("test code")
-		s.o.print("test code")
-		s.o.print("test code")
-		s.o.print("test code")
-		s.o.print("test code")
-	}
-}
-class Test
-{
-	void Add()
-	{
-		c=a+b
-	}
-	void sub()
-	{
-		c=a-b()
-	}
-}
-
-
-class demo.java
+public static void main
 {
 	public static void main()
 	{
